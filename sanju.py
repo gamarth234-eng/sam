5822f0de9c5f047728010399b594ea69c96ae8a7
samarth123 <gamarth234@gmail.com>
2025-08-19 12:08:31 +0530

    Add files via upload

---
+++ sanju.py
@@ -0,0 +1,13 @@
+print("list")
+l1=[1,2,3,"abc",4,"xfdd",20.3]
+print(l1)
+print("dictionary")
+d1={"a":135,"b":125,"c":369}
+print(d1)
+print("tuples")
+t1=(10,20,30,40,50,40)
+print(t1)
+print("sets")
+s1={10,10,20,30,30,50,30,50,50,}
+print(s1)
+ 
